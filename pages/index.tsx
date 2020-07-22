d63fd9a07aeaa5eb88a69a868af7ccb262bb2dd9
Fabian Vera <fvera@comparaonline.com>
2020-07-21 23:56:48 -0400

    fixed initial

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
+import React from "react";
 import Head from "next/head";
 import ProjectPreview, { ProjectPreviewEntry } from "../components/ProjectPreview";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
 //import { useRouter } from "next/router";
 import useSWR from "swr";
-import fetcherFunction from '../utilities/fetcher';
+import fetcherFunction from "../utilities/fetcher";
 
 const renderProjects = () => {
   //const { query } = useRouter();
@@ -22,31 +23,27 @@ const renderProjects = () => {
   const projects = data.data;
   console.log(projects);
 
-  const output = projects.map((entry: ProjectPreviewEntry) => {
-    return (
-      <li key={entry.id}>
-        <ProjectPreview entry={entry} />
-      </li>
-    );
-  });
+  const output = projects.map((entry: ProjectPreviewEntry) => (
+    <li key={entry.id}>
+      <ProjectPreview entry={entry} />
+    </li>
+  ));
 
-  return <ul className='flex'>{output}</ul>;
+  return <ul className="flex">{output}</ul>;
 };
 
-const index = () => {
-  return (
-    <>
-      <Head>
-        <title>Hola Soy Vale</title>
-      </Head>
-      <main className='container mx-auto'>
-        <Header>
-          <Nav />
-        </Header>
-        {renderProjects()}
-      </main>
-    </>
-  );
-};
+const index = () => (
+  <>
+    <Head>
+      <title>Hola Soy Vale</title>
+    </Head>
+    <main className="container mx-auto">
+      <Header>
+        <Nav />
+      </Header>
+      {renderProjects()}
+    </main>
+  </>
+);
 
 export default index;
